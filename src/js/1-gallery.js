d2f7a4c46329749dde216c8a43fc154fdff1821c
OleksiyGromov <gromov196312@gmail.com>
2024-11-17 19:28:07 +0200

    gallerymarkup

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -65,8 +65,11 @@ const data = [
   },
 ];
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const gallery = document.querySelector('.gallery');
 
